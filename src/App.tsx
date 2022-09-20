1f8ae682a1bbbcd805078dfabb4e9248febb1edf
Ebenezr <ebenezarbukosia@gmail.com>
2022-09-20 18:34:02 +0300

    skills section ui styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,119 @@
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import mockup from "./assets/mockupclock.png";
+import react from "./assets/react.png";
+import rails from "./assets/rails.png";
+import ts from "./assets/ts.png";
+import ruby from "./assets/ruby.png";
+import sass from "./assets/sass.png";
+import js from "./assets/javascript.png";
+import ProgressBar from "@ramonak/react-progress-bar";
 function App() {
   return (
     <main>
-      <header>
+      <header className="main-header">
         <nav>
-          <h2>00 ABOUT</h2>
-          <h2>01 SKILLS</h2>
-          <h2>02 EXP</h2>
-          <h2>03 CONTACT</h2>
+          <h2 className="nav-links">00 ABOUT</h2>
+          <h2 className="nav-links">01 SKILLS</h2>
+          <h2 className="nav-links">02 EXP</h2>
+          <h2 className="nav-links">03 CONTACT</h2>
         </nav>
       </header>
       <section className="about">
         <header>
+          <small className="section-name">About</small>
           <h1 className="section-title">EBENEZAR BUKOSIA</h1>
         </header>
       </section>
       <section className="skills">
         <header>
-          <h1 className="section-title">SKILLS</h1>
+          <small className="section-name">SKILLS</small>
         </header>
         <article className="content">
           <p>I have a wide range of skills</p>
-          <div className="list-skils">
-            <ul>
-              <li>React</li>
-              <li>HTML</li>
-              <li>Javascript</li>
-              <li>Typescript</li>
-              <li>CSS</li>
-              <li>Sass</li>
-            </ul>
-            <ul>
-              <li>Ruby On Rails</li>
-              <li>Ruby</li>
-              <li>SQL</li>
-              <li>Tailwind</li>
-              <li>ES6</li>
-              <li>Node</li>
-            </ul>
+          <div className="list-skills">
+            <div className="skill-card">
+              <small>React</small>
+              <img src={react} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={90}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#00d1f7"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
+            <div className="skill-card">
+              <small>Sass</small>
+              <img src={sass} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={95}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#c76395"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
+            <div className="skill-card">
+              <small>Ruby On Rails</small>
+              <img src={rails} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={95}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#a62c46"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
+            <div className="skill-card">
+              <small>Javascript</small>
+              <img src={js} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={75}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#efd81d"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
+
+            <div className="skill-card">
+              <small>Typescript</small>
+              <img src={ts} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={70}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#2f74c0"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
+            <div className="skill-card">
+              <small>Ruby</small>
+              <img src={ruby} alt="" />
+              <ProgressBar
+                isLabelVisible={false}
+                completed={80}
+                width="100%"
+                customLabel=""
+                height="5px"
+                bgColor="#da0c00"
+                baseBgColor="rgb(39, 39, 39)"
+              />
+            </div>
           </div>
         </article>
       </section>
-      <section className="exp">
+      <section className="work">
         <header>
           <small className="section-name">Work</small>
           <h2 className="section-title">
@@ -49,8 +122,13 @@ function App() {
         </header>
         <article className="content">
           <div className="navigation">
-            <button className="btn"></button>
-            <button className="btn"></button>
+            <button className="btn nav-btn">
+              <IoIosArrowBack />
+            </button>
+            4
+            <button className="btn nav-btn">
+              <IoIosArrowForward />
+            </button>
           </div>
           <div className="project-wrapper">
             <h3 className="project-name">Clock-me</h3>
@@ -61,7 +139,9 @@ function App() {
             </p>
             <button className="btn btn-main">View project</button>
           </div>
-          <div className="project-poster"></div>
+          <div className="project-poster">
+            <img src={mockup} alt="" />
+          </div>
         </article>
       </section>
       <section className="contact">
